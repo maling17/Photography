4608a611f29564d633f2bc293edb438f0d952162
Adytia Kresna Farisi <kresnaa09@yahoo.com>
2019-02-09 12:41:54 +0700

    initial commit

---
+++ app/src/main/java/com/example/kresna/photography/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.kresna.photography;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("pesan", "masuk Oncreate ");
+
+        RecyclerView recyclerView = findViewById(R.id.Recyler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
