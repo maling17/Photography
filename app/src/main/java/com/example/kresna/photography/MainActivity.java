ceea09583e9174dfaab2359989bba4dd1511927e
Adytia Kresna Farisi <kresnaa09@yahoo.com>
2019-02-09 14:13:41 +0700

    menambahkan progresbar kedalam xml activity_main dan menambahkan method untuk memunculkan/menghilangkan loading.

--- app/src/main/java/com/example/kresna/photography/MainActivity.java
+++ app/src/main/java/com/example/kresna/photography/MainActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout relativeLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("pesan", "masuk Oncreate ");
 
+        relativeLoading = findViewById(R.id.RelativeLoading);
         RecyclerView recyclerView = findViewById(R.id.Recyler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        DataDummy dataDummy=new DataDummy();
-        RecylerAdapter adapter=new RecylerAdapter(dataDummy);
+        DataDummy dataDummy = new DataDummy();
+        RecylerAdapter adapter = new RecylerAdapter(dataDummy);
         recyclerView.setAdapter(adapter);
 
+    }
+
+    private void showLoading() {
+        relativeLoading.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoading() {
+        relativeLoading.setVisibility(View.GONE);
     }
 }
